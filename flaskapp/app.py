4d2c3739f107a0d5101435f61cea0b46c9d8f091
root <root@localhost.localdomain>
2021-11-08 19:49:27 +0100

    mongoengine example

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,14 +1,48 @@
 import os
 from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
+#from mongoengine import StringField, Document, connect
 
 application = Flask(__name__)
-
 application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ[
-    'MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
+    'MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':' + os.environ['MONGODB_PORT'] + '/' + os.environ['MONGODB_DATABASE']
 
-mongo = PyMongo(application)
-db = mongo.db
+# connect(host=application.config["MONGO_URI"])
+
+
+# class User(Document):
+#     name = StringField(required=True)
+#     email = StringField()
+
+
+# class Place(Document):
+#     name = StringField(required=True)
+#     address = StringField(required=True)
+
+
+# @application.route("/place/new")
+# def new_place():
+#     """Creates a new place
+
+#     e.g: GET /place/new?name=Restaurant1
+#     """
+
+#     place = Place(name=request.args.get("name"),
+#                   address=request.args.get("address"))
+#     place.save()
+#     print(place.name, place.address)
+#     return "Place saved"
+
+
+# @application.route("/places")
+# def list_places():
+#     """List places.
+
+#     e.g: GET /places """
+
+#     places = list(Place.objects)
+#     places = map(lambda x: "<li>" + x.name +
+#                  ", " + x.address + "</li>", places)
+#     return "<p>Places</p><ul> {} </ul>".format("\n".join(places))
 
 
 @application.route('/')
@@ -16,12 +50,12 @@ def index():
     application.logger.info("GOT A REQUEST!")
     return jsonify(
         status=True,
-        message='Welcome to the Auberdine Flask app!'
+        message='Welcome to the Auberdine Flask application!'
     )
 
 
 if __name__ == "__main__":
-    ENV_DEBUG = os.environ.get("APP_DEBUG", True)
-    ENV_PORT = os.environ.get("APP_PORT", 5000)
+    ENV_DEBUG = os.environ.get("APPLICATION_DEBUG", True)
+    ENV_PORT = os.environ.get("APPLICATION_PORT", 5000)
 
     application.run(host='0.0.0.0', port=ENV_PORT, debug=ENV_DEBUG)
