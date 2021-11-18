7d89f0d232e834c1c529ad8ab5fc8e79e8bffe03
Németh Krisztián <nemethk@ukatemi.com>
2021-11-18 11:04:20 +0100

    Removed some leftover stuff

--- flaskapp/authorization.py
+++ flaskapp/authorization.py
@@ -4,18 +4,22 @@ from flaskapp.models.user import User
 jwt = JWTManager()
 
 
-# Register a callback function that takes whatever object is passed in as the
-# identity when creating JWTs and converts it to a JSON serializable format.
 @jwt.user_identity_loader
 def user_identity_lookup(user):
+    """
+    Register a callback function that takes whatever object is passed in as the
+    identity when creating JWTs and converts it to a JSON serializable format.
+    """
     return str(user.id)
 
 
-# Register a callback function that loads a user from your database whenever
-# a protected route is accessed. This should return any python object on a
-# successful lookup, or None if the lookup failed for any reason (for example
-# if the user has been deleted from the database).
 @jwt.user_lookup_loader
 def user_lookup_callback(_jwt_header, jwt_data):
+    """
+    Register a callback function that loads a user from your database whenever
+    a protected route is accessed. This should return any python object on a
+    successful lookup, or None if the lookup failed for any reason (for example
+    if the user has been deleted from the database).
+    """
     identity = jwt_data["sub"]
     return User.objects.get(id=identity)
