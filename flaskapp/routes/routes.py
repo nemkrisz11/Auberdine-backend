ba711d1b86f89fe192fb057dff17add2f98fb882
Németh Krisztián <nemethk@ukatemi.com>
2021-11-19 12:10:38 +0100

    Added profile GET (get reviews of user).

--- flaskapp/routes/routes.py
+++ flaskapp/routes/routes.py
@@ -3,6 +3,7 @@ from flaskapp.routes.user.login import LoginApi
 from flaskapp.routes.user.logout import LogoutApi
 from flaskapp.routes.user.register import RegisterApi
 from flaskapp.routes.user.properties import PropertiesApi
+from flaskapp.routes.user.profile import ProfileApi
 from flaskapp.routes.place.debug_place import DebugPlaceApi
 
 
@@ -11,5 +12,6 @@ def initialize_routes(api):
     api.add_resource(LoginApi, '/user/login')
     api.add_resource(LogoutApi, '/user/logout')
     api.add_resource(PropertiesApi, '/user/properties')
-    api.add_resource(IndexApi, '/')
+    api.add_resource(ProfileApi, '/user/<user_id>')
+    api.add_resource(IndexApi, '/')  # TODO: Temporary URL
     api.add_resource(DebugPlaceApi, "/place/debug_places")
---
+++ flaskapp/routes/user/profile.py
@@ -0,0 +1,34 @@
+from flask import jsonify
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required
+
+from flaskapp.models.user import User
+from flaskapp.models.review import Review
+from flaskapp.models.place import Place
+
+
+class ProfileApi(Resource):
+    @jwt_required()
+    def get(self, user_id):
+        """
+        Fetches the user profile
+        e.g: GET /user/<user_id>
+        """
+        user = User.objects.get(id=user_id)
+        user_reviews = Review.objects(user_id=user_id)
+
+        user_reviewed_places = []
+        for review in user_reviews:
+            place = Place.objects.get(id=review.place_id)
+            user_reviewed_places.append({
+                "place_id": review.place_id,
+                "place_name": place.name,
+                "address": place.address,
+                "rating": review.rating,
+                "text": review.text,
+            })
+
+        return jsonify({
+            "name": user.name,
+            "reviews": user_reviewed_places
+        })
